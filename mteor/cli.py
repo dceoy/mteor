9bd6118422f220517cda042de33d862779060934
dceoy <dnarsil+github@gmail.com>
2022-05-12 05:22:42 +0900

    Implement ohlc command

--- mteor/cli.py
+++ mteor/cli.py
@@ -7,6 +7,9 @@ Usage:
   mteor --version
   mteor mt5 [--debug|--info] [--mt5-exe=<path>] [--mt5-login=<str>]
     [--mt5-password=<str>] [--mt5-server=<str>]
+  mteor ohlc [--debug|--info] [--mt5-exe=<path>] [--mt5-login=<str>]
+    [--mt5-password=<str>] [--mt5-server=<str>] [--granularity=<str>]
+    [--count=<int>] <instrument>
 
 Options:
   -h, --help            Print help and exit
@@ -16,12 +19,18 @@ Options:
   --mt5-login=<str>     Specify a MT5 trading account number
   --mt5-password=<str>  Specify a MT5 trading account password
   --mt5-server=<str>    Specify a MT5 trade server name
+  --granularity=<str>   Specify a timeframe granularity [default: M1]
+  --count=<int>         Specify a record count [default: 10]
+
+Arguments:
+  <instrument>          Financial instrument symbol
 """
 
 import logging
 import os
 
 import MetaTrader5 as Mt5
+import pandas as pd
 from docopt import docopt
 
 from . import __version__
@@ -35,9 +44,37 @@ def main():
     _initialize_mt5(args=args)
     if args['mt5']:
         _print_mt5_info()
+    elif args['ohlc']:
+        _print_ohlc(
+            symbol=args['<instrument>'], granularity=args['--granularity'],
+            count=int(args['--count'])
+        )
     else:
         pass
     Mt5.shutdown()
+
+
+def _print_ohlc(symbol, granularity, count, start_pos=0,
+                display_max_columns=500, display_width=1500):
+    pd.set_option('display.max_columns', display_max_columns)
+    pd.set_option('display.max_rows', count)
+    pd.set_option('display.width', display_width)
+    print(
+        _fetch_df_rate(
+            symbol=symbol, granularity=granularity, count=count,
+            start_pos=start_pos
+        ).reset_index().to_string(index=False)
+    )
+
+
+def _fetch_df_rate(symbol, granularity, count, start_pos=0):
+    return pd.DataFrame(
+        Mt5.copy_rates_from_pos(
+            symbol, getattr(Mt5, f'TIMEFRAME_{granularity}'), start_pos, count
+        )
+    ).assign(
+        time=lambda d: pd.to_datetime(d['time'], unit='s')
+    ).set_index('time')
 
 
 def _print_mt5_info():
@@ -46,7 +83,7 @@ def _print_mt5_info():
     print('Terminal version:\t{}'.format(Mt5.version()))
     print('Terminal status and settings:\t{}'.format(Mt5.terminal_info()))
     print('Trading account info:\t{}'.format(Mt5.account_info()))
-    print('Financial instruments:\t{}'.format(Mt5.symbols_total()))
+    print('Number of financial instruments:\t{}'.format(Mt5.symbols_total()))
 
 
 def _initialize_mt5(args):
