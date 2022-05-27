6469624d16e72febb1e3bae7a6fc9d268a5478c6
dceoy <dnarsil+github@gmail.com>
2022-05-27 18:25:42 +0900

    Fix bugs

--- mteor/signal.py
+++ mteor/signal.py
@@ -26,44 +26,47 @@ class SignalDetector(object):
             df_sig.iloc[-1].to_dict() if df_sig.shape[0] > 0
             else {c: np.nan for c in df_sig.columns}
         )
-        emlrr_ci = self._calculate_ci(
+        self.__logger.debug(f'sig: {sig}')
+        lrr_ema_ci = self._calculate_ci(
             alpha=(1 - self.significance_level), df=(self.signal_ema_span - 1),
             loc=sig['lrr_ema'], scale=sig['lrr_emse']
         )
-        emsr_ci = self._calculate_ci(
-            alpha=(1 - self.significance_level), df=(self.signal_ema_span - 1),
-            loc=sig['sr_ema'], scale=sig['sr_emse']
+        self.__logger.debug(f'lrr_ema_ci: {lrr_ema_ci}')
+        sr_ema_ci = self._calculate_ci(
+            alpha=(1 - self.significance_level),
+            df=(self.signal_ema_span - 1), loc=sig['sr_ema'],
+            scale=sig['sr_emse']
         )
-        if emlrr_ci[0] > 0 and emsr_ci[0] > 0:
+        self.__logger.debug(f'sr_ema_ci: {sr_ema_ci}')
+        if sr_ema_ci[0] > 0 and sig['lrr_ema'] > 0:
             act = 'long'
-        elif emlrr_ci[1] < 0 and emsr_ci[1] < 0:
+        elif sr_ema_ci[1] < 0 and sig['lrr_ema'] < 0:
             act = 'short'
-        elif ((position_side == 'short'
-               and ((emlrr_ci[0] > 0 and sig['sr_ema'] > 0)
-                    or (emsr_ci[0] > 0 and sig['lrr_ema'] > 0)))
-              or (position_side == 'long'
-                  and ((emlrr_ci[1] < 0 and sig['sr_ema'] < 0)
-                       or (emsr_ci[1] < 0 and sig['lrr_ema'] < 0)))):
+        elif ((position_side == 'short' and lrr_ema_ci[0] > 0
+               and sig['sr_ema'] > 0)
+              or (position_side == 'long' and lrr_ema_ci[1] < 0
+                  and sig['sr_ema'] < 0)):
             act = 'closing'
         else:
             act = None
+        self.__logger.debug(f'act: {act}')
         return {
-            'act': act, **sig,
-            'emlrr_ci_lower': emlrr_ci[0],
-            'emlrr_ci_upper': emlrr_ci[1],
-            'emsr_ci_lower': emsr_ci[0], 'emsr_ci_upper': emsr_ci[1],
-            'log_str': '{0:^38}|{1:^34}|'.format(
-                'EMLRR:{0:>10}{1:>18}'.format(
-                    '{:.1g}'.format(emlrr_ci.mean()),
+            'act': act, **sig, 'lrr_ema_ci_lower': lrr_ema_ci[0],
+            'lrr_ema_ci_upper': lrr_ema_ci[1], 'sr_ema_ci_lower': sr_ema_ci[0],
+            'sr_ema_ci_upper': sr_ema_ci[1],
+            'log_str': '{0:^36}|{1:^32}|'.format(
+                'LRR:{0:>10}{1:>18}'.format(
+                    '{:.1g}'.format(sig['lrr_ema']),
                     np.array2string(
-                        emlrr_ci,
+                        lrr_ema_ci,
                         formatter={'float_kind': lambda f: f'{f:.1g}'}
                     )
                 ),
-                'EMSR:{0:>9}{1:>16}'.format(
-                    '{:.1g}'.format(emsr_ci.mean()),
+                'SR:{0:>9}{1:>16}'.format(
+                    '{:.1g}'.format(sig['sr_ema']),
                     np.array2string(
-                        emsr_ci, formatter={'float_kind': lambda f: f'{f:.1g}'}
+                        sr_ema_ci,
+                        formatter={'float_kind': lambda f: f'{f:.1g}'}
                     )
                 )
             )
@@ -71,11 +74,13 @@ class SignalDetector(object):
 
     @staticmethod
     def _calculate_log_return_rate(df_tick, span):
-        return df_tick.reset_index().assign(
+        return df_tick.assign(
             mid=lambda d: d[['ask', 'bid']].mean(axis=1),
-            delta_sec=lambda d: d['time_msc'].diff().dt.total_seconds()
-        ).set_index('time_msc').assign(
-            lrr=lambda d: (np.log(d['mid']).diff() / d['delta_sec'])
+            delta_sec=lambda d: d.index.to_series().diff().dt.total_seconds()
+        ).assign(
+            log_return=lambda d: np.log(d['mid']).diff()
+        ).assign(
+            lrr=lambda d: (d['log_return'] / d['delta_sec'])
         ).assign(
             lrr_ema=lambda d: d['lrr'].ewm(span=span, adjust=False).mean(),
             lrr_emse=lambda d:
@@ -85,17 +90,20 @@ class SignalDetector(object):
     @staticmethod
     def _calculate_sharpe_ratio(df_lrr, span):
         return df_lrr.assign(
-            return_rate=lambda d: (np.exp(d['log_return']) - 1),
+            return_per_sec=lambda d: (np.exp(d['lrr']) - 1),
             spread_ratio=lambda d: (1 - ((d['ask'] - d['bid']) / d['mid']))
         ).assign(
-            sr=lambda d: (
-                d['return_rate'] * d['spread_ratio']
-                / d['return_rate'].rolling(window=span).std(ddof=1)
+            sharpe_ratio=lambda d: (
+                d['return_per_sec'] * d['spread_ratio']
+                / d['return_per_sec'].rolling(window=span).std(ddof=1)
             )
         ).assign(
-            sr_ema=lambda d: d['sr'].ewm(span=span, adjust=False).mean(),
-            sr_emse=lambda d:
-            np.sqrt(d['sr'].ewm(span=span, adjust=False).var(ddof=1) / span)
+            sr_ema=lambda d:
+            d['sharpe_ratio'].ewm(span=span, adjust=False).mean(),
+            sr_emse=lambda d: np.sqrt(
+                d['sharpe_ratio'].ewm(span=span, adjust=False).var(ddof=1)
+                / span
+            )
         )
 
     @staticmethod
